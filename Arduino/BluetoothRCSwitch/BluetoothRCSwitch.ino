2303a24742483da81488812d847069360945a1af
Tunjid Dahunsi <tjdah100@gmail.com>
2017-03-12 00:56:53 -0500

    Fixed Arduino sketch code, commited the wrong sketch by mistake.

--- Arduino/BluetoothRCSwitch/BluetoothRCSwitch.ino
+++ Arduino/BluetoothRCSwitch/BluetoothRCSwitch.ino
@@ -6,7 +6,8 @@
 #include "Wire.h"
 #include "Math.h"
 #include "BGLib.h" // BGLib C library for BGAPI communication.
-#include <RCSwitch.h>
+#include "RCSwitch.h"
+#include "FlexiTimer2.h"
 
 // uncomment the following line for debug serial output
 #define DEBUG
@@ -15,38 +16,10 @@
 // Constants
 // ================================================================
 
-// ================================================================
-// Dynamic variables
-// ================================================================
-
-uint8_t state; // State of RC Switch.
-
-uint8_t bioImpData[11]; // Unsigned integer array to carry data to phone.
-
-uint8_t sampleRateArray[2] = {
-        sampleRate, 0}; // Initialize default sample rate value holder.
-
-uint8_t freqSweepArray[4] = {
-        startFreq, stepSize, numOfIncrements, 0}; // Initialize default frquency sweep value holder.
-
-boolean NOTIFICATIONS_FLAG = false; // Variable that toggles notifications to phone subscription state. 
-
-boolean TIMED_OUT_FLAG = false; // Used to identify if the module timed out.
-
-boolean FREQ_SWEEP_FLAG = false; // Used to toggle frequency sweeps.
-
-volatile
-boolean SAMPLE_RATE_FLAG = false;  // Variable to manage sample rate. Managed from interrupt context.
-
-// ================================================================
-// BLE STATE TRACKING 
-// ================================================================
-
 // RC Switch state machine definitions
 
-#define STATE_IDLE          0
-#define STATE_SNIFFING      1
-#define STATE_SENDING       2
+#define STATE_SNIFFING              0
+#define STATE_SENDING               1
 
 // BLE state machine definitions
 
@@ -57,6 +30,19 @@ boolean SAMPLE_RATE_FLAG = false;  // Variable to manage sample rate. Managed fr
 #define BLE_STATE_CONNECTED_MASTER  4
 #define BLE_STATE_CONNECTED_SLAVE   5
 #define BLE_STATE_TIMED_OUT         6
+
+// ================================================================
+// Dynamic variables
+// ================================================================
+
+volatile uint8_t state; // State of RC Switch.
+
+boolean TIMED_OUT_FLAG = false; // Used to identify if the module timed out.
+
+// ================================================================
+// BLE STATE TRACKING 
+// ================================================================
+
 
 // BLE state/link status tracker
 
@@ -71,9 +57,9 @@ uint8_t ble_bonding = 0xFF; // 0xFF = no bonding, otherwise = bonding handle
 #define LED_PIN         13  // Arduino Uno LED pin
 #define BLE_RESET_PIN   6   // BLE reset pin (active-low)
 
-#define GATT_HANDLE_C_BIOIMPEDANCE_DATA   17  // 0x11, supports "read", "notify" and "indicate" operations
-#define GATT_HANDLE_C_SAMPLE_RATE   21  // 0x15, supports "read" and "write" operations
-#define GATT_HANDLE_C_AC_FREQ   25  // 0x19, supports "read" and "write" operations
+#define GATT_HANDLE_C_STATE_TOGGLE   17  // 0x11, supports "read", "notify" and "indicate" operations
+#define GATT_HANDLE_C_SNIFFER   21  // 0x15, supports "read" and "write" operations
+#define GATT_HANDLE_C_TRANSMITTER   25  // 0x19, supports "read" and "write" operations
 
 // use SoftwareSerial on pins D8/D8 for RX/TX (Arduino side)
 
@@ -91,8 +77,10 @@ BGLib ble112((HardwareSerial * ) & Serial1, 0, 1);
 
 
 // The interrupt number, on the MicroView this is PIN 11. On Arduino it is PIN 2. See http://arduino.cc/en/Reference/AttachInterrupt
-#define INTERRUPT_RX 0
+#define INTERRUPT_RX digitalPinToInterrupt(3)
+#define PIN_TX 5
 
+uint8_t stateCallback[1];
 RCSwitch receiveSwitch = RCSwitch();
 
 // ================================================================
@@ -104,8 +92,6 @@ RCSwitch receiveSwitch = RCSwitch();
 void setup() {
 
     Wire.begin(); // Start Arduino I2C library
-
-    receiveSwitch.enableReceive(INTERRUPT_RX);
 
     // ================================================================
     // For BLE, Serial Communication and Timer interrupts
@@ -149,25 +135,10 @@ void setup() {
     delay(5); // wait 5ms
     digitalWrite(BLE_RESET_PIN, HIGH);
 
-    Serial.print("Start freq: ");
-    Serial.print(startFreqHz);
+    Serial.print("Starting up");
     Serial.println();
 
-    Serial.print("System Phase Shift: ");
-    Serial.print(systemPhaseShift, 4);
-    Serial.println();
-
-    // Write default values for handles once.
-    ble112.ble_cmd_attributes_write(GATT_HANDLE_C_SAMPLE_RATE, 0, 1, sampleRateArray);
-
-    delay(20); // Wait 20 ms so async callback isn't blocked.
-
-    ble112.ble_cmd_attributes_write(GATT_HANDLE_C_AC_FREQ, 0, 3, freqSweepArray);
-
-    CR_Array = new double[1];
-    CR_Array[0] = cal_resistance; // initialize CR array;
-
-    state = STATE_IDLE;
+    state = STATE_SENDING;
 }
 
 // ================================================================
@@ -184,8 +155,11 @@ void loop() {
     switch (state) {
 
         case STATE_SNIFFING:
-            if (receiveSwitch.available()) {
-                int value = receiveSwitch.getReceivedValue();
+            if (receiveSwitch.available()) { 
+                // Stop timer interrupts
+                FlexiTimer2::stop();
+                
+                unsigned long value = receiveSwitch.getReceivedValue();
 
                 if (value == 0) {
                     Serial.println("Unknown encoding");
@@ -200,50 +174,27 @@ void loop() {
                     Serial.println(receiveSwitch.getReceivedProtocol());
                     Serial.print("Delay (Pulse Length): ");
                     Serial.println(receiveSwitch.getReceivedDelay());
+
+                    // Break long value to binary byte array representation
+                    uint8_t bytes[6];
+
+                    bytes[0] = (value >> 24) & 0xff;
+                    bytes[1] = (value >> 16) & 0xff;
+                    bytes[2] = (value >> 8) & 0xff;
+                    bytes[3] = value & 0xff;
+                    bytes[4] = receiveSwitch.getReceivedDelay();
+                    bytes[5] = receiveSwitch.getReceivedBitlength();
+                    
+                    // Write value to characteristic on ble112. Causes indication to be sent.
+                    ble112.ble_cmd_attributes_write(GATT_HANDLE_C_SNIFFER, 0, 6, bytes);
+                    
+                    // Revert state to sending
+                    state = STATE_SENDING;
                 }
 
                 receiveSwitch.resetAvailable();
             }
             break;
-    }
-
-    // Check if GATT Client (Smartphone) is subscribed to notifications.
-
-    if (SAMPLE_RATE_FLAG) { // this flag is toggled from the timer's interrupt context. It controls the sample rate.
-
-        // For BLE
-        // ===================================================
-
-        /*bioImpData[0] = (35 * sin((pseudo*3.14)/180));
-         bioImpData[1] = (35 * cos((pseudo*3.14)/180));
-         bioImpData[2] = (0);*/
-        lsm.read();
-        bioImpData[0] = (int) (lsm.accelData.x / 10);
-        bioImpData[1] = (int) (lsm.accelData.y / 10);
-        bioImpData[2] = (int) (lsm.accelData.z / 10);
-
-        if (Z_Value > 7000) {
-
-            for (int i = 3; i <= 7; i++) {    // Transmit zero if filter is not satisfied.
-                bioImpData[i] = 0;
-            }
-
-        }
-        else {
-            Z_Value += 0.00005;
-            Z_Value *= 10000;
-            phaseAngle += 0.00005;
-            phaseAngle *= 10000;
-            updateData((long) Z_Value, (long) phaseAngle, bioImpData);
-        }
-
-        //Write notification values to characteristic on ble112. Causes notification to be sent.
-        ble112.ble_cmd_attributes_write(GATT_HANDLE_C_BIOIMPEDANCE_DATA, 0, 11, bioImpData);
-        SAMPLE_RATE_FLAG = false; // Switch this flag back to false till timer interrupt switches it back on.
-    }
-    else {
-        // Do zilch, zip, nada, nothing if notifications are not enabled.
-        //AD5933.setCtrMode(POWER_DOWN, ctrReg);
     }
 
     // blink Arduino LED based on state:
@@ -362,11 +313,9 @@ void my_ble_evt_system_boot(const ble_msg_system_boot_evt_t *msg) {
     // build custom scan response data (i.e. the Device Name value)
     // default BLE stack value: 140942474c69622055314131502033382e344e4657
     uint8 sr_data[] = {
-            0x14, // field length
+            0xD, // field length
             BGLIB_GAP_AD_TYPE_LOCALNAME_COMPLETE, // field type
-            'Z', '_', 'B', 'L', 'E', ' ', 'D', '3', 'm', 'o', ' ',
-            //'M', 'E', 'G', 'A', ' ',
-            '0', '0', ':', '0', '0', ':', '0', '0'
+            'B', 'L', 'E', ' ', 'R', 'C', ' ', 'S', 'w', 'i', 't', 'c', 'h'
     };
 
     // get BLE MAC address
@@ -507,198 +456,49 @@ void my_ble_evt_attributes_value(const struct ble_msg_attributes_value_evt_t *ms
 #endif
 
     // check for data written to "c_sample_rate" handle
-    if (msg->handle == GATT_HANDLE_C_SAMPLE_RATE && msg->value.len > 0) {
+    if (msg->handle == GATT_HANDLE_C_STATE_TOGGLE && msg->value.len > 0) {
 
-        sampleRate = msg->value.data[0];
-        sampleRatePeriod = 1000000 / ((long) sampleRate);
+        state = msg->value.data[0];
 
-        sampleRateArray[0] = sampleRate;
-        sampleRateArray[1] = 1;
+        // Send indication acknowledging the write
+        stateCallback[0] = STATE_SNIFFING;
+        ble112.ble_cmd_attributes_write(GATT_HANDLE_C_STATE_TOGGLE, 0, 1, stateCallback);
 
-        Serial.println();
-        Serial.print("Sucessful write attempt; new frequency / period: ");
-        Serial.print(sampleRate);
-        Serial.print(" hertz");
-        Serial.print(" / ");
-        Serial.print(sampleRatePeriod);
-        Serial.print(" microseconds.");
-        Serial.println();
+        Serial.print("Current state is: ");
+        Serial.println(state);
 
-        ble112.ble_cmd_attributes_write(GATT_HANDLE_C_SAMPLE_RATE, 0, 2, sampleRateArray);
+        switch (state){
+            case STATE_SNIFFING:
+                receiveSwitch.enableReceive(INTERRUPT_RX);
 
-        Micro40Timer::set(sampleRatePeriod, notify);
-        Micro40Timer::start();
+                // Only sniff for 5 seconds or till a packet is received.
+                FlexiTimer2::set(5000, notify);
+                FlexiTimer2::start();
+                break;
+            case STATE_SENDING:
+                receiveSwitch.enableTransmit(PIN_TX);
+                break;
+        }
     }
-    // check for data written to "c_ac_freq" handle
-    if (msg->handle == GATT_HANDLE_C_AC_FREQ && msg->value.len > 0) {
+    else if (msg->handle == GATT_HANDLE_C_TRANSMITTER && msg->value.len > 0) {
+      uint8_t* b = msg->value.data;
 
-        delete[] GF_Array; // Free memory from previous GF array.
-        delete[] PS_Array; // Free memory from previous PS array.
-        delete[] CR_Array; // Free memory from previous CR array.
+      unsigned long code = ((uint32_t)b[0] << 24) | ((uint32_t)b[1] << 16) | ((uint32_t)b[2] << 8) | ((uint32_t)b[3]);
 
-        Serial.println("Old Arrays Deleted.");
+      int pulseLength = msg->value.data[4];
+      int bitLength  = msg->value.data[5];
+      
+      Serial.print("Code received: ");
+      Serial.println(code);
+      Serial.print("Pulse Length: ");
+      Serial.println(pulseLength);
+      Serial.print("Bit Length: ");
+      Serial.println(bitLength);
 
-        startFreq = msg->value.data[0];
-        stepSize = msg->value.data[1];
-        numOfIncrements = msg->value.data[2];
-        freqSweepArray[0] = startFreq;
-        freqSweepArray[1] = stepSize;
-        freqSweepArray[2] = numOfIncrements;
-        freqSweepArray[3] = 1;
-
-        bioImpData[10] = startFreq; // update AC freq value
-        currentStep = 0; // reset currentStep for filtering in loop.
-
-        startFreqHz = (double) startFreq * 1000;
-
-        if (stepSize == 0) { // frequency sweep is disabled
-
-            FREQ_SWEEP_FLAG = false;
-
-            CR_Array = new double[1]; // size of one for single impedance at single frequency.
-
-            Serial.println("New Array created.");
-
-            AD5933.setExtClock(false);
-            AD5933.resetAD5933();
-            AD5933.setSettlingCycles(cycles_base, cycles_multiplier);
-            AD5933.setStartFreq(startFreqHz);
-            AD5933.setVolPGA(0, 1);
-            AD5933.getGainFactorC(cal_resistance, cal_samples, gain_factor, systemPhaseShift, false);
-
-            AD5933.getComplex(gain_factor, systemPhaseShift, CR_Array[0], phaseAngle);
-
-            Serial.println("Gain factors gotten.");
-
-            Serial.println();
-            Serial.print("Frequency:");
-            Serial.print("\t");
-            Serial.print(startFreqHz);
-            Serial.print("\t");
-            Serial.print("Gain factor:");
-            Serial.print("\t");
-            Serial.print(gain_factor);
-            Serial.print("\t");
-            Serial.print("SystemPS:");
-            Serial.print("\t");
-            Serial.print(systemPhaseShift, 4);
-            Serial.print("\t");
-            Serial.print("CR: ");
-            Serial.print("\t");
-            Serial.print(CR_Array[0]);
-            Serial.println();
-        }
-        else { // frequency sweep is enabled
-
-            FREQ_SWEEP_FLAG = true;
-
-            stepSizeHz = (double) stepSize * 1000;
-            endFreqHz = startFreqHz + ((double) stepSize * ((double) numOfIncrements) * 1000);
-
-            // generate gain factor array using two point calibration.
-
-            GF_Array = new double[numOfIncrements + 1];
-            PS_Array = new double[numOfIncrements + 1];
-            CR_Array = new double[numOfIncrements + 1];
-
-            deltaGF = 0;
-            deltaPS = 0;
-
-            for (int i = 0; i < numOfIncrements; i++) {
-                GF_Array[i] = 0;    // Initialize all elements to zero.
-                PS_Array[i] = 0;    // Initialize all elements to zero.
-            }
-
-            Serial.println("New Arrays created.");
-
-            AD5933.setExtClock(false);
-            AD5933.resetAD5933();
-            AD5933.setStartFreq(startFreqHz);
-            AD5933.setIncrement(stepSizeHz);
-            AD5933.setNumofIncrement(numOfIncrements);
-            AD5933.setSettlingCycles(cycles_base, cycles_multiplier);
-            AD5933.getTemperature();
-            AD5933.setVolPGA(0, 1);
-
-            /*AD5933.getGainFactors_LI(
-             cal_resistance, cal_samples, startFreqHz, endFreqHz,
-             GF_Array[0], GF_Array[numOfIncrements],
-             PS_Array[0], PS_Array[numOfIncrements]);
-
-             deltaGF = GF_Array[numOfIncrements] - GF_Array[0];
-             deltaPS = PS_Array[numOfIncrements] - PS_Array[0];
-
-             AD5933.getArraysLI(
-             deltaGF, deltaPS,
-             stepSizeHz, numOfIncrements,
-             GF_Array[0], PS_Array[0],
-             GF_Array, PS_Array);*/
-
-            AD5933.getGainFactorS_Set(cal_resistance, cal_samples, GF_Array, PS_Array);
-
-            Serial.println("Gain factors gotten.");
-
-            Serial.println();
-
-            for (int i = 0; i <= numOfIncrements; i++) { // print and set CR filter array.
-
-                if (i == 0) {
-                    ctrReg = AD5933.getByte(0x80);
-                    AD5933.setCtrMode(STAND_BY, ctrReg);
-                    AD5933.setCtrMode(INIT_START_FREQ, ctrReg);
-                    AD5933.setCtrMode(START_FREQ_SWEEP, ctrReg);
-                    AD5933.getComplex(GF_Array[i], PS_Array[i], CR_Array[i], phaseAngle);
-                }
-
-                else if (i > 0 && i < numOfIncrements) {
-                    AD5933.getComplex(GF_Array[i], PS_Array[i], CR_Array[i], phaseAngle);
-                    AD5933.setCtrMode(INCR_FREQ, ctrReg);
-                }
-
-                else if (i = numOfIncrements) {
-                    AD5933.getComplex(GF_Array[i], PS_Array[i], CR_Array[i], phaseAngle);
-                    AD5933.setCtrMode(POWER_DOWN, ctrReg);
-                }
-
-                Serial.print("Frequency: ");
-                Serial.print("\t");
-                Serial.print(startFreqHz + (stepSizeHz * i));
-                Serial.print("\t");
-                Serial.print("Gainfactor term: ");
-                Serial.print(i);
-                Serial.print("\t");
-                Serial.print(GF_Array[i]);
-                Serial.print("\t");
-                Serial.print("SystemPS term: ");
-                Serial.print(i);
-                Serial.print("\t");
-                Serial.print(PS_Array[i], 4);
-                Serial.print("\t");
-                Serial.print("CR term: ");
-                Serial.print(i);
-                Serial.print("\t");
-                Serial.print(CR_Array[i]);
-                Serial.println();
-            }
-        }
-        Serial.println();
-        Serial.print("Sucessful write attempt to c_ac_freq.");
-        Serial.println();
-        Serial.print("Start Frequency (KHz): ");
-        Serial.print(startFreq);
-        Serial.println();
-        Serial.print("Step size (KHz): ");
-        Serial.print(stepSize);
-        Serial.println();
-        Serial.print("Number of increments: ");
-        Serial.print(numOfIncrements);
-        Serial.println();
-        Serial.println();
-
-        ble112.ble_cmd_attributes_write(GATT_HANDLE_C_AC_FREQ, 0, 4, freqSweepArray);
-
-        Micro40Timer::set(sampleRatePeriod, notify);
-        Micro40Timer::start();
+      receiveSwitch.enableTransmit(PIN_TX);
+      receiveSwitch.setProtocol(1);
+      receiveSwitch.setPulseLength(pulseLength + 6);
+      receiveSwitch.send(code, bitLength);
     }
 }
 
@@ -733,48 +533,16 @@ void my_ble_evt_attributes_status(const struct ble_msg_attributes_status_evt_t *
     Serial.print(msg->handle, DEC);
     Serial.println(" }");
 #endif
-
-    if (msg->flags == 1) {
-        NOTIFICATIONS_FLAG = true;
-    }
-    else if (msg->flags == 0) {
-        NOTIFICATIONS_FLAG = false;
-    }
-    else {
-        NOTIFICATIONS_FLAG = false;
-    }
 }
 
 void notify() {
-    if (NOTIFICATIONS_FLAG) {
-        SAMPLE_RATE_FLAG = true;
-    }
-}
+    FlexiTimer2::stop();
+    Serial.println("Timer interrupt called");
+    state = STATE_SENDING;
+    receiveSwitch.enableTransmit(PIN_TX);
 
-void updateData(long magnitude, long phaseAng, uint8_t *values) {
-
-    if (phaseAng > 0) {
-        values[9] = phaseAng % 100;
-        phaseAng /= 100;
-        values[8] = phaseAng % 100;
-        phaseAng /= 100;
-        values[7] = phaseAng;
-    }
-    else {
-        phaseAng *= -1;
-        values[9] = -1 * (phaseAng % 100);
-        phaseAng /= 100;
-        values[8] = phaseAng % 100;
-        phaseAng /= 100;
-        values[7] = phaseAng;
-    }
-    values[6] = magnitude % 100;
-    magnitude /= 100;
-    values[5] = magnitude % 100;
-    magnitude /= 100;
-    values[4] = magnitude % 100;
-    magnitude /= 100;
-    values[3] = magnitude % 100;
+    stateCallback[0] = STATE_SENDING;
+    ble112.ble_cmd_attributes_write(GATT_HANDLE_C_STATE_TOGGLE, 0, 1, stateCallback);
 }
 
 void resetBLE() {

59609d81aae6bf00e3180b25d9659f76b0f80b34
Adetunji Dahunsi <tjdah100@gmail.com>
2019-06-19 23:41:52 -0400

    More error logging

--- app/src/main/java/com/tunjid/rcswitchcontrol/abstractclasses/BaseFragment.kt
+++ app/src/main/java/com/tunjid/rcswitchcontrol/abstractclasses/BaseFragment.kt
@@ -1,6 +1,7 @@
 package com.tunjid.rcswitchcontrol.abstractclasses
 
 
+import android.util.Log
 import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat.getDrawable
@@ -56,7 +57,7 @@ abstract class BaseFragment : com.tunjid.androidbootstrap.core.abstractclasses.B
 
     fun onInconsistentList(exception: Exception) {
         hostingActivity.recreate()
-        exception.printStackTrace()
+        Log.i("RecyclerView", "Inconsistency in ${javaClass.simpleName}", exception)
     }
 
     fun togglePersistentUi() {

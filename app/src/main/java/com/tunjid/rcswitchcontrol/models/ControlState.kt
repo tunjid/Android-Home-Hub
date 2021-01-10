65c4bfb2913f81fbedd6fbca599aac45e793c2ed
Adetunji Dahunsi <tjdah100@gmail.com>
2021-01-10 11:19:56 -0500

    Pull out reduction of  control state into the control state class

--- app/src/main/java/com/tunjid/rcswitchcontrol/models/ControlState.kt
+++ app/src/main/java/com/tunjid/rcswitchcontrol/models/ControlState.kt
@@ -1,17 +1,31 @@
 package com.tunjid.rcswitchcontrol.models
 
+import android.content.Context
 import android.content.res.Resources
 import androidx.fragment.app.Fragment
+import com.jakewharton.rx.replayingShare
+import com.rcswitchcontrol.protocols.CommonDeviceActions
 import com.rcswitchcontrol.protocols.CommsProtocol
 import com.rcswitchcontrol.protocols.Name
 import com.rcswitchcontrol.protocols.models.Payload
 import com.rcswitchcontrol.zigbee.models.ZigBeeAttribute
 import com.rcswitchcontrol.zigbee.models.ZigBeeCommandInfo
+import com.rcswitchcontrol.zigbee.models.ZigBeeNode
+import com.rcswitchcontrol.zigbee.protocol.ZigBeeProtocol
 import com.tunjid.rcswitchcontrol.R
+import com.tunjid.rcswitchcontrol.a433mhz.models.RfSwitch
+import com.tunjid.rcswitchcontrol.a433mhz.protocols.BLERFProtocol
+import com.tunjid.rcswitchcontrol.a433mhz.protocols.SerialRFProtocol
+import com.tunjid.rcswitchcontrol.a433mhz.services.ClientBleService
+import com.tunjid.rcswitchcontrol.common.deserialize
+import com.tunjid.rcswitchcontrol.common.deserializeList
 import com.tunjid.rcswitchcontrol.fragments.DevicesFragment
 import com.tunjid.rcswitchcontrol.fragments.HostFragment
 import com.tunjid.rcswitchcontrol.fragments.RecordFragment
 import com.tunjid.rcswitchcontrol.utils.Tab
+import io.reactivex.Flowable
+import io.reactivex.rxkotlin.Flowables
+import io.reactivex.schedulers.Schedulers
 import java.util.HashMap
 import java.util.Locale
 
@@ -22,15 +36,6 @@ data class ProtocolKey(val key: CommsProtocol.Key) : Tab {
 
     override fun createFragment(): Fragment = RecordFragment.commandInstance(this)
 }
-
-data class ControlState(
-    val isNew: Boolean = false,
-    val connectionState: String = "",
-    val commandInfo: ZigBeeCommandInfo? = null,
-    val history: List<Record> = listOf(),
-    val commands: Map<CommsProtocol.Key, List<Record.Command>> = mapOf(),
-    val devices: List<Device> = listOf()
-)
 
 enum class Page : Tab {
 
@@ -49,19 +54,48 @@ enum class Page : Tab {
     }
 }
 
+data class ControlState(
+    val isNew: Boolean = false,
+    val connectionState: String = "",
+    val commandInfo: ZigBeeCommandInfo? = null,
+    val history: List<Record> = listOf(),
+    val commands: Map<CommsProtocol.Key, List<Record.Command>> = mapOf(),
+    val devices: List<Device> = listOf()
+)
+
+fun controlState(
+    status: Flowable<String>,
+    payloads: Flowable<Payload>
+) = Flowables.combineLatest(
+    status,
+    payloads
+).scan(ControlState()) { state, (connectionState, payload) ->
+    val key = payload.key
+    val isNew = !state.commands.keys.contains(key)
+
+    state.copy(isNew = isNew, connectionState = connectionState, commandInfo = payload.extractCommandInfo)
+        .reduceCommands(payload)
+        .reduceDeviceName(payload.deviceName)
+        .reduceHistory(payload.extractRecord)
+        .reduceDevices(payload.extractDevices)
+        .reduceZigBeeAttributes(payload.extractDeviceAttributes)
+}
+    .subscribeOn(Schedulers.single())
+    .replayingShare()
+
 val ControlState.keys
     get() = commands.keys
         .sortedBy(CommsProtocol.Key::value)
         .map(::ProtocolKey)
 
-fun ControlState.reduceDevices(fetched: List<Device>?) = when {
+private fun ControlState.reduceDevices(fetched: List<Device>?) = when {
     fetched != null -> copy(devices = (fetched + devices)
         .distinctBy(Device::diffId)
         .sortedBy(Device::name))
     else -> this
 }
 
-fun ControlState.reduceZigBeeAttributes(fetched: List<ZigBeeAttribute>?) = when (fetched) {
+private fun ControlState.reduceZigBeeAttributes(fetched: List<ZigBeeAttribute>?) = when (fetched) {
     null -> this
     else -> copy(devices = devices
         .filterIsInstance<Device.ZigBee>()
@@ -71,18 +105,18 @@ fun ControlState.reduceZigBeeAttributes(fetched: List<ZigBeeAttribute>?) = when 
     )
 }
 
-fun ControlState.reduceHistory(record: Record?) = when {
+private fun ControlState.reduceHistory(record: Record?) = when {
     record != null -> copy(history = (history + record).takeLast(500))
     else -> this
 }
 
-fun ControlState.reduceCommands(payload: Payload) = copy(
+private fun ControlState.reduceCommands(payload: Payload) = copy(
     commands = HashMap(commands).apply {
         this[payload.key] = payload.commands.map { Record.Command(key = payload.key, command = it) }
     }
 )
 
-fun ControlState.reduceDeviceName(name: Name?) = when (name) {
+private fun ControlState.reduceDeviceName(name: Name?) = when (name) {
     null -> this
     else -> copy(devices = listOfNotNull(devices.firstOrNull { it.id == name.id }.let {
         when (it) {
@@ -95,3 +129,47 @@ fun ControlState.reduceDeviceName(name: Name?) = when (name) {
         .distinctBy(Device::diffId)
     )
 }
+
+private val Payload.deviceName: Name?
+    get() = when (action) {
+        CommonDeviceActions.nameChangedAction -> data?.deserialize(Name::class)
+        else -> null
+    }
+private val Payload.extractRecord: Record.Response?
+    get() = response.let {
+        if (it == null || it.isBlank()) null
+        else Record.Response(key = key, entry = it)
+    }
+
+private val Payload.extractCommandInfo: ZigBeeCommandInfo?
+    get() = if (key == ZigBeeProtocol.key && action == ZigBeeProtocol.commandInfoAction) data?.deserialize(ZigBeeCommandInfo::class)
+    else null
+
+private val Payload.extractDevices: List<Device>?
+    get() = when (val serialized = data) {
+        null -> null
+        else -> when (key) {
+            BLERFProtocol.key, SerialRFProtocol.key -> when (action) {
+                ClientBleService.transmitterAction,
+                CommonDeviceActions.deleteAction,
+                CommonDeviceActions.renameAction -> serialized.deserializeList(RfSwitch::class)
+                    .map(Device::RF)
+                else -> null
+            }
+            ZigBeeProtocol.key -> when (action) {
+                CommonDeviceActions.refreshDevicesAction -> serialized.deserializeList(ZigBeeNode::class)
+                    .map(Device::ZigBee)
+                else -> null
+            }
+            else -> null
+        }
+    }
+
+private val Payload.extractDeviceAttributes: List<ZigBeeAttribute>?
+    get() = when (key) {
+        ZigBeeProtocol.key -> when (action) {
+            in ZigBeeProtocol.attributeCarryingActions -> data?.deserializeList(ZigBeeAttribute::class)
+            else -> null
+        }
+        else -> null
+    }
--- app/src/main/java/com/tunjid/rcswitchcontrol/viewmodels/ControlViewModel.kt
+++ app/src/main/java/com/tunjid/rcswitchcontrol/viewmodels/ControlViewModel.kt
@@ -27,39 +27,20 @@ package com.tunjid.rcswitchcontrol.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import com.jakewharton.rx.replayingShare
-import com.rcswitchcontrol.protocols.CommonDeviceActions
 import com.rcswitchcontrol.protocols.CommsProtocol
-import com.rcswitchcontrol.protocols.Name
 import com.rcswitchcontrol.protocols.models.Payload
-import com.rcswitchcontrol.zigbee.models.ZigBeeAttribute
-import com.rcswitchcontrol.zigbee.models.ZigBeeCommandInfo
-import com.rcswitchcontrol.zigbee.models.ZigBeeNode
-import com.rcswitchcontrol.zigbee.protocol.ZigBeeProtocol
 import com.tunjid.androidx.core.components.services.HardServiceConnection
 import com.tunjid.rcswitchcontrol.R
-import com.tunjid.rcswitchcontrol.a433mhz.models.RfSwitch
-import com.tunjid.rcswitchcontrol.a433mhz.protocols.BLERFProtocol
-import com.tunjid.rcswitchcontrol.a433mhz.protocols.SerialRFProtocol
-import com.tunjid.rcswitchcontrol.a433mhz.services.ClientBleService
 import com.tunjid.rcswitchcontrol.common.Broadcaster
 import com.tunjid.rcswitchcontrol.common.deserialize
-import com.tunjid.rcswitchcontrol.common.deserializeList
 import com.tunjid.rcswitchcontrol.common.toLiveData
 import com.tunjid.rcswitchcontrol.models.ControlState
 import com.tunjid.rcswitchcontrol.models.Device
 import com.tunjid.rcswitchcontrol.models.Page
-import com.tunjid.rcswitchcontrol.models.Record
-import com.tunjid.rcswitchcontrol.models.reduceCommands
-import com.tunjid.rcswitchcontrol.models.reduceDeviceName
-import com.tunjid.rcswitchcontrol.models.reduceDevices
-import com.tunjid.rcswitchcontrol.models.reduceHistory
-import com.tunjid.rcswitchcontrol.models.reduceZigBeeAttributes
+import com.tunjid.rcswitchcontrol.models.controlState
 import com.tunjid.rcswitchcontrol.services.ClientNsdService
 import com.tunjid.rcswitchcontrol.services.ServerNsdService
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.Flowables
-import io.reactivex.schedulers.Schedulers.single
 
 class ControlViewModel(app: Application) : AndroidViewModel(app) {
 
@@ -90,23 +71,11 @@ class ControlViewModel(app: Application) : AndroidViewModel(app) {
             .filter(String::isNotBlank)
             .map { it.deserialize(Payload::class) }
 
-        val stateObservable = Flowables.combineLatest(
+        val stateObservable = controlState(
+            connectionStatuses,
             serverResponses,
-            connectionStatuses
-        ).scan(ControlState()) { state, (payload, connectionState) ->
-            val key = payload.key
-            val isNew = !state.commands.keys.contains(key)
-
-            state.copy(isNew = isNew, connectionState = connectionState, commandInfo = payload.extractCommandInfo)
-                .reduceCommands(payload)
-                .reduceDeviceName(payload.deviceName)
-                .reduceHistory(payload.extractRecord)
-                .reduceDevices(payload.extractDevices)
-                .reduceZigBeeAttributes(payload.extractDeviceAttributes)
-        }
-            .subscribeOn(single())
+        )
             .doOnSubscribe { nsdConnection.boundService?.onAppForeGround() }
-            .replayingShare()
 
         state = stateObservable.toLiveData()
 
@@ -179,47 +148,3 @@ private val connectionActions = arrayOf(
     ClientNsdService.ACTION_SOCKET_DISCONNECTED,
     ClientNsdService.ACTION_START_NSD_DISCOVERY
 )
-
-private val Payload.deviceName: Name?
-    get() = when (action) {
-        CommonDeviceActions.nameChangedAction -> data?.deserialize(Name::class)
-        else -> null
-    }
-private val Payload.extractRecord: Record.Response?
-    get() = response.let {
-        if (it == null || it.isBlank()) null
-        else Record.Response(key = key, entry = it)
-    }
-
-private val Payload.extractCommandInfo: ZigBeeCommandInfo?
-    get() = if (key == ZigBeeProtocol.key && action == ZigBeeProtocol.commandInfoAction) data?.deserialize(ZigBeeCommandInfo::class)
-    else null
-
-private val Payload.extractDevices: List<Device>?
-    get() = when (val serialized = data) {
-        null -> null
-        else -> when (key) {
-            BLERFProtocol.key, SerialRFProtocol.key -> when (action) {
-                ClientBleService.transmitterAction,
-                CommonDeviceActions.deleteAction,
-                CommonDeviceActions.renameAction -> serialized.deserializeList(RfSwitch::class)
-                    .map(Device::RF)
-                else -> null
-            }
-            ZigBeeProtocol.key -> when (action) {
-                CommonDeviceActions.refreshDevicesAction -> serialized.deserializeList(ZigBeeNode::class)
-                    .map(Device::ZigBee)
-                else -> null
-            }
-            else -> null
-        }
-    }
-
-private val Payload.extractDeviceAttributes: List<ZigBeeAttribute>?
-    get() = when (key) {
-        ZigBeeProtocol.key -> when (action) {
-            in ZigBeeProtocol.attributeCarryingActions -> data?.deserializeList(ZigBeeAttribute::class)
-            else -> null
-        }
-        else -> null
-    }

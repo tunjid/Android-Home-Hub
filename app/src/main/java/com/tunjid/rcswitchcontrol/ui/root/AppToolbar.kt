716c86e281145497b3e9e63cdb41dcbca7b2960e
Adetunji Dahunsi <tjdah100@gmail.com>
2021-09-12 14:53:00 -0400

    Add app toolbar

--- app/src/main/java/com/tunjid/rcswitchcontrol/ui/root/AppToolbar.kt
+++ app/src/main/java/com/tunjid/rcswitchcontrol/ui/root/AppToolbar.kt
@@ -1,26 +1,22 @@
 package com.tunjid.rcswitchcontrol.ui.root
 
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import com.tunjid.globalui.ToolbarItem
 import com.tunjid.globalui.ToolbarState
 import com.tunjid.rcswitchcontrol.di.AppDependencies
@@ -28,27 +24,50 @@ import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 internal fun BoxScope.AppToolbar(stateFlow: StateFlow<ToolbarState>) {
+    val scope = rememberCoroutineScope()
     val state by stateFlow.collectAsState()
     val alpha: Float by animateFloatAsState(if (state.visible) 1f else 0f)
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .offset(y = with(LocalDensity.current) { state.statusBarSize.toDp() })
+            .height(56.dp)
+            .alpha(alpha)
+            .align(Alignment.TopCenter)
+            .fillMaxWidth(),
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            text = state.toolbarTitle.toString(),
+            color = MaterialTheme.colors.onPrimary
+        )
+        ActionMenu(
+            stateFlow = stateFlow.mapState(
+                scope = scope,
+                mapper = ToolbarState::items
+            )
+        )
+    }
+}
+
+@Composable
+internal fun ActionMenu(stateFlow: StateFlow<List<ToolbarItem>>) {
+    val flow = remember { stateFlow }
+    val items by flow.collectAsState()
     val icons = when {
-        state.items.size < 3 -> state.items
-        else -> state.items.take(2)
+        items.size < 3 -> items
+        else -> items.take(2)
     }
 
-    TopAppBar(
-        title = { Text(text = state.toolbarTitle.toString()) },
-        modifier = Modifier
-            .alpha(alpha)
-            .background(color = Color.Transparent)
-            .align(Alignment.TopCenter)
-            .padding(top = with(LocalDensity.current) { state.statusBarSize.toDp() })
-            .wrapContentHeight()
-            .fillMaxWidth(),
-        navigationIcon = null,
-        actions = {
-            icons.map { ToolbarIcon(item = it) }
-        }
-    )
+    Row(
+        horizontalArrangement = Arrangement.End,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        icons.forEach { ToolbarIcon(it) }
+    }
 }
 
 @Composable
@@ -62,21 +81,27 @@ fun ToolbarIcon(item: ToolbarItem) {
 
     when (val vector = item.imageVector) {
         null -> TextButton(
-            onClick = {
-                println("Click $item")
-                clicks(item)
-            },
+            modifier = Modifier
+                .wrapContentSize(align = Alignment.CenterEnd)
+                .padding(horizontal = 4.dp),
+            onClick = { clicks(item) },
             content = {
-                Text(text = item.text)
+                Text(
+                    text = item.text,
+                    color = MaterialTheme.colors.onPrimary
+                )
             }
         )
         else -> IconButton(
+            modifier = Modifier
+                .wrapContentSize(align = Alignment.CenterEnd)
+                .padding(horizontal = 4.dp),
             onClick = { clicks(item) }
         ) {
             Icon(
                 imageVector = vector,
                 contentDescription = item.text,
-                tint = MaterialTheme.colors.onSecondary
+                tint = MaterialTheme.colors.onPrimary
             )
         }
     }
